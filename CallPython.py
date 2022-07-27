dc116c93ccdab965c11b301dfc2576335a8b2279
sifacaii <46623793+sifacaii@users.noreply.github.com>
2022-07-27 10:59:54 +0800

    Update CallPython.py

--- CallPython.py
+++ CallPython.py
@@ -1,3 +1,24 @@
-def CallPython(n):
-	a = n + "sdfjklsdj"
-	return a
+def CallPython(args):
+	result = {
+		'code':1,
+		'msg':'',
+		'data':''
+	}
+	try:
+		import sys
+		sys.path.append(r"D:\pyscript")
+		import fibmodule
+		result['data'] = fibmodule.fengfa(args)
+
+	except Exception as exmsg:
+		result['code'] = 1
+		result['msg'] = str(exmsg)
+	else:
+		result['code'] = 0
+		result['msg'] = '执行成功'
+
+	import json
+	return json.dumps(result,ensure_ascii=False)
+
+if __name__ == "__main__":
+	print(CallPython("aaa"))
